98589e0cb16e6d1a7315068ecd96ba9e842e594d
GabrielSlPires <gabriel.slpires@gmail.com>
2023-04-04 17:43:15 -0300

    add shiny busy function

--- shiny/ui.R
+++ shiny/ui.R
@@ -1,6 +1,17 @@
 library(shiny)
 library(shinydashboard)
 
+shiny_busy <- function() { #https://community.rstudio.com/t/shiny-app-show-some-message-while-user-is-waiting-for-output/12822
+  # use &nbsp; for some alignment, if needed
+  HTML("&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;", paste0(
+    '<span data-display-if="',
+    '$(&#39;html&#39;).attr(&#39;class&#39;)==&#39;shiny-busy&#39;',
+    '">',
+    '<i class="fa fa-spinner fa-pulse fa-fw" style="color:orange"></i>',
+    '</span>'
+  ))
+}
+
 sidebar <- dashboardSidebar(
   sidebarMenu(
     menuItem("Running Sensors",
@@ -29,6 +40,7 @@ body <- dashboardBody(
         column(
           width = 6,
           actionButton("btn_refreash_data", "Refreash Data"),
+          shiny_busy()
         )
       ),
       fluidRow(
