c07be0085aef2ffa34ef5054e25625f80a05f18d
GabrielSlPires <gabriel.slpires@gmail.com>
2023-05-24 15:57:19 -0300

    update shinyapps.io comand

--- run.R
+++ run.R
@@ -4,3 +4,5 @@ rstudioapi::jobRunScript("shiny/read_iotree.R")
 rstudioapi::jobRunScript("shiny/run_app.R")
 
 #rsconnect::deployApp(appDir = "shiny/", server = "shinyapps.io", appName = "iotree")
+
+#use mvtsplot() tipo um mapa de calor para time series pg 7 do livro  
